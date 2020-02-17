cb016152b513cf49b80a7dc79df6d8d500b9cc86
E-Ward <s182964@ucs.ac.uk>
2020-02-17 03:30:04 +0000

    Latest build

---
+++ Final Project (Dissertation)/Final Project/Assets/Scripts/Modular_3D_Player_Controller.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Modular_3D_Player_Controller : MonoBehaviour
+{
+    
+    [Header("Movement Keys")]
+    [Tooltip("Here you can select which keys you would like to use for movement.")] //This text will appear when you hover over the header 
+    public KeyCode forward;
+    public KeyCode backward;
+    public KeyCode left;
+    public KeyCode right;
+    public KeyCode jump;
+    public KeyCode sprint;
+    public KeyCode crouch;
+    public KeyCode cameraChange;
+
+    [Header("Movement Variables")]
+    [Tooltip("This value determines how fast the character will walk")]
+    public float walkingSpeed;
+    float currentSpeed;
+    [Tooltip("This value determines how fast the character will walk when running")]
+    public float sprintSpeed;
+    [Tooltip("This value determines how fast the character will walk when crouched")]
+    public float crouchSpeed;
+    public bool isCrouching;
+    [Tooltip("This value is how much gravity is applied to the player determining how fast they will fall")]
+    public float gravity;
+    [Tooltip("This value determines how high the character will jump")]
+    public float jumpForce;
+    public bool isGrounded;
+    float distToGround;
+    public bool isFirstPerson;
+
+    [Header("Attributes")]
+    [Tooltip("This value determines how much health the player has")]
+    public int Health;
+    [Tooltip("This value determines how much stamina the player has")]
+    public float Stamina;
+
+    [Header("Scripts")]
+    public mouseLook MouseScript;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Sets the default camera position to be first person
+        isFirstPerson = true;
+        // Sets the current speed of the player = to the walking speed set by the user
+        currentSpeed = walkingSpeed;
+        // Sets the distance to the ground by
+        distToGround = GetComponent<Collider>().bounds.extents.y;
+        // Sets the is crouching to false so the player starts standing
+        isCrouching = false;
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    //Vector3 velocity;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(forward))
+        {
+            transform.position += transform.forward * currentSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(backward))
+        {
+            transform.position += -transform.forward * currentSpeed * Time.deltaTime;
+        }
+            
+        if (Input.GetKey(left))
+        {
+            transform.position += -transform.right * currentSpeed * Time.deltaTime;
+        }
+            
+        if (Input.GetKey(right))
+        {
+            transform.position += transform.right * currentSpeed * Time.deltaTime;
+        }
+   
+        if (Input.GetKeyDown(jump) && isGrounded == true)
+        {
+
+            rb.velocity = Vector3.up * jumpForce * Time.deltaTime;
+            //transform.position += Vector3.up * jumpForce * Time.deltaTime;
+        }
+        
+        if (Input.GetKeyDown(sprint))
+        {
+            currentSpeed = sprintSpeed;
+        }
+        else if (Input.GetKeyUp(sprint) )
+        {
+            currentSpeed = walkingSpeed;
+        }
+
+        if (Input.GetKeyDown(crouch))
+        {
+            //MouseScript.MoveCameraToCrouchPos();
+            isCrouching = true;
+            currentSpeed = crouchSpeed;
+        }
+        else if(Input.GetKeyUp(crouch))
+        {
+            //MouseScript.MoveCameraToStandPos();
+            isCrouching = false;
+            currentSpeed = walkingSpeed;
+        }
+
+        if(isGrounded == false)
+        {
+            transform.position += Vector3.down * gravity * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(cameraChange) && isFirstPerson == true)
+        {
+            isFirstPerson = false;
+        }
+        else if (Input.GetKeyDown(cameraChange) && isFirstPerson == false)
+        {
+            isFirstPerson = true;
+        }
+    }
+    void FixedUpdate()
+    {
+        if (!Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f))
+        {
+            isGrounded = false;
+        }
+        else
+        {
+            isGrounded = true;
+        }
+    }
+}
---
+++ Final Project (Dissertation)/Final Project/Assets/Scripts/mouseLook.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 300f;
+
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+    [Header("Camera Positons")]
+    public Transform endmarker = null;
+    public Transform topMarker = null;
+    public Transform thirdPersonMarker = null;
+
+    [Header("Scripts")]
+    public Modular_3D_Player_Controller PlayerControllerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = topMarker.position;
+        // Locks the cursor to the middle of the screen
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+
+        if (PlayerControllerScript.isCrouching == true)
+        {
+            transform.position = Vector3.Lerp(transform.position, endmarker.position, Time.deltaTime);
+        }
+        else if (PlayerControllerScript.isCrouching == false && PlayerControllerScript.isFirstPerson == true)
+        {
+            transform.position = Vector3.Lerp(transform.position, topMarker.position, Time.deltaTime);
+        }
+
+        //if (PlayerControllerScript.isFirstPerson == true)
+        //{
+           // transform.position = Vector3.Lerp(transform.position, topMarker.position, Time.deltaTime);
+        //}
+        if (PlayerControllerScript.isFirstPerson == false)
+        {
+            transform.position = Vector3.Lerp(transform.position, thirdPersonMarker.position, Time.deltaTime);
+        }
+
+    }
+}
